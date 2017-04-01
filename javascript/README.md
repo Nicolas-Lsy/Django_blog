e1fdd92ff1387f21f001f8b315c82831da18f155
pywizard <pywizard6261@gmail.com>
2017-04-01 23:55:27 +0800

    增加JS 1

---
+++ javascript/README.md
@@ -0,0 +1,4 @@
+JavaScript  已经无处不在了  
+现在学习一些简单的JS 
+
+-- 完成hello world ！
---
+++ javascript/app.js
@@ -0,0 +1,24 @@
+document.write('hello , world , There a math question. as follows:    ');
+
+a = 2;
+b = 3242; 
+
+document.write("a * b = ") 
+document.write(a * b ) 
+
+
+function hello(){
+	return document.write("   This come from a Fun");
+
+}
+
+
+hello();
+
+function sin_fun(degree){
+	return document.write(Math.sin(degree)); 
+	
+}
+sin(43); 
+
+
